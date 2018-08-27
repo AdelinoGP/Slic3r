0a1e1ff5f75db119cbebede3e36cb45558b4cee7
Daniel Ahlers <2ahlers@informatik.uni-hamburg.de>
2018-08-27 09:33:01 +0200

    added collision detection for nonplanar surfaces

--- lib/Slic3r/Print/Object.pm
+++ lib/Slic3r/Print/Object.pm
@@ -160,6 +160,7 @@ sub detect_nonplanar_surfaces {
     $self->slice;
 
     # Detect nonplanar surfaces areas and move them to the heighest layer of the nonplanar_surface
+    $self->print->status_cb->(25, "Move nonplanar surfaces up");
     $self->move_nonplanar_surfaces_up;
 }
 
@@ -205,7 +206,7 @@ sub prepare_infill {
     $self->combine_infill;
 
     $self->set_step_done(STEP_PREPARE_INFILL);
-    $self->debug_svg_print;
+    #$self->debug_svg_print;
 }
 
 sub infill {
@@ -216,6 +217,7 @@ sub infill {
 
     $self->_infill;
     
+    $self->print->status_cb->(35, "Project nonplanar surfaces down");
     $self->project_nonplanar_surfaces;
 }
 
