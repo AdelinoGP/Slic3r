121744a2cc4bf466871a351c5f1b864f3634999a
Daniel Ahlers <ahlers@informatik.uni-hamburg.de>
2020-10-13 15:43:14 +0200

    rewritten move nonplanar_surfaces_up

--- xs/src/libslic3r/LayerRegion.cpp
+++ xs/src/libslic3r/LayerRegion.cpp
@@ -287,7 +287,7 @@ LayerRegion::infill_area_threshold() const
 }
 
 void
-LayerRegion::append_nonplanar_surface(NonplanarSurface& surface)
+LayerRegion::append_nonplanar_surface(NonplanarSurface& surface, float distance_to_top)
 {
     for(auto & s : this->nonplanar_surfaces){
         if (s == surface){
@@ -295,6 +295,7 @@ LayerRegion::append_nonplanar_surface(NonplanarSurface& surface)
         }    
     }
     nonplanar_surfaces.push_back(surface);
+    distances_to_top.push_back(distance_to_top);
 }
 
 void
